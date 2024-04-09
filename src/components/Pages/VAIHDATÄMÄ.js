e3f95fbf277c0d243c4ebbb11684dde9ef785542
Joonaket <104364418+Joonaket@users.noreply.github.com>
2024-04-09 15:10:35 +0300

    Testing

--- src/components/Pages/VAIHDATÄMÄ.js
+++ src/components/Pages/VAIHDATÄMÄ.js
@@ -2,15 +2,15 @@ let paikallinenIP;
 
 const baseIP = "http://localhost";
 
-let sinunIP = "";        //Laita tämän muuttujan arvoksi oman koneen IP-osoite jos halut kokeilla sivua mobiililla, ilman tätä API-kutsut ei toimi kuin localhostilla
+let sinunIP = "moosebackend-js7ux2g4rq-lz.a.run.app";        //Laita tämän muuttujan arvoksi oman koneen IP-osoite jos halut kokeilla sivua mobiililla, ilman tätä API-kutsut ei toimi kuin localhostilla
 
 let vaihdettavaIP = "http://"+sinunIP;
 
 if (vaihdettavaIP === "http://") {
-    paikallinenIP = baseIP + ":8081";
+    paikallinenIP = baseIP + ":8080";
 }
 else{
-    paikallinenIP = vaihdettavaIP+":8081";
+    paikallinenIP = vaihdettavaIP+":8080";
 }
 
 
